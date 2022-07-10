5b59c756eae304753d16d597faff4410d1e6345c
Rating001 <106230549+Rating001@users.noreply.github.com>
2022-07-09 22:51:31 -0700

    Created the Invoice

---
+++ js/site.js
@@ -0,0 +1,10 @@
+function printInvoice(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
